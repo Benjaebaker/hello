604fa931b847be442316f0c01c5efd6d865573bb
Vadim Derun <vadimderun@yahoo.com>
2019-01-17 15:35:19 -0600

    added name

--- hello.c
+++ hello.c
@@ -11,7 +11,7 @@
 int main(int argc, char **argv) {
 
   printf("Hello World!\n");
-  printf("Ben BAker & Joel Hernandez");
+  printf("Ben BAker, Vadim Derun & Joel Hernandez");
   printf("Computer engneering");
   return 0;
 }
